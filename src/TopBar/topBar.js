e828e85ca3a5465da87f962159eaadfe3ce2dec2
RxRay <3136613819@qq.com>
2024-01-27 20:44:58 +0800

    添加登录后的界面

--- src/TopBar/topBar.js
+++ src/TopBar/topBar.js
@@ -1,14 +1,21 @@
 import "./css/topbar.css"
 import "./css/dropdown.css"
-import logo from "./img/logos/logo-l2.png"
+import logo from "./img/logos/logo-l3.png"
 import header from "./img/header/header.png"
+import mheader from "./img/header/mheader.png"
+import fheader from "./img/header/fheader.png"
+
 
 export function TopBar() {
     function LogOut(){
         window.location.href="/login";
     }
 
-    function UserDropdown() {
+    function UserDropdown(){
+        return NoInfo();
+    }
+
+    function NoInfo() {
         return <div className="right-function">
             <img src={header} alt="" className="dropdown-btn"/>
             <div className="dropdown-content">
@@ -16,6 +23,32 @@ export function TopBar() {
                     <li className="gap">尚未登录</li>
                     <li onClick={LogOut}>登录</li>
                     <li>注册</li>
+                </ul>
+            </div>
+        </div>
+    }
+
+    function Male() {
+        return <div className="right-function">
+            <img src={mheader} alt="" className="dropdown-btn"/>
+            <div className="dropdown-content">
+                <ul>
+                    <li className="gap">路人甲</li>
+                    <li onClick={LogOut}>个人主页</li>
+                    <li>退出账号</li>
+                </ul>
+            </div>
+        </div>
+    }
+
+    function Female() {
+        return <div className="right-function">
+            <img src={fheader} alt="" className="dropdown-btn"/>
+            <div className="dropdown-content">
+                <ul>
+                    <li className="gap">路人乙</li>
+                    <li onClick={LogOut}>个人主页</li>
+                    <li>退出账号</li>
                 </ul>
             </div>
         </div>
