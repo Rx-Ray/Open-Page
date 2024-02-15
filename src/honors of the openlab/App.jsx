4f66e15ad5c6b87bf5b92c73185c77c7c9583a87
miaoiaoaoa <149806474+miaoiaoaoa@users.noreply.github.com>
2024-02-15 16:40:15 +0800

    Update App.jsx

--- src/honors of the openlab/App.jsx
+++ src/honors of the openlab/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
-
+import { FloatButton } from 'antd';
+import {  CommentOutlined,CloudServerOutlined,HomeOutlined,UsergroupAddOutlined} from '@ant-design/icons';
 const images = [
   'https://via.placeholder.com/150',
   'https://via.placeholder.com/150',
@@ -32,6 +33,18 @@ function App() {
         <div className="circle circle3"></div>
         <div className="circle circle4"></div>
         <div className="circle circle5"></div>
+      <FloatButton.Group
+        shape="circle"
+        style={{
+          right: 24,
+          top:1,
+        }}
+      >
+        <FloatButton icon={<HomeOutlined />} tooltip={'个人主页'} href='###' target='_self' badge={{color:'blue'}}></FloatButton>
+        <FloatButton icon={<CloudServerOutlined />} tooltip={"往期资料"} href='###'target='_blank' badge={{color:'blue'}}></FloatButton>
+        <FloatButton  icon={<CommentOutlined />} tooltip={"OpenPage入口"} href='###' target='_blank' badge={{color:'blue'}}></FloatButton>
+        <FloatButton icon={<UsergroupAddOutlined />} tooltip={"组队界面"} href="###" target='_blank' badge={{color:'blue'}}></FloatButton>
+      </FloatButton.Group>
       </div>
     </div>
   );
